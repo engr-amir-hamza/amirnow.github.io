cef48aaa3686ce5151c0e832b74944563a8afdd2
Amir Hamza <120237699+engr-amir-hamza@users.noreply.github.com>
2023-01-07 12:51:41 +0500

    m

---
+++ wp-content/plugins/master-addons/inc/modules/header-footer-comment/inc/view/modal-header.php
@@ -0,0 +1,11 @@
+<div class="jltma-pop-contents-head">
+    <div class="jltma-popup-head-content float-left">
+        <h3>
+            <br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function esc_html__() in D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\view\modal-header.php:4
+Stack trace:
+#0 {main}
+  thrown in <b>D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\view\modal-header.php</b> on line <b>4</b><br>
+</h3>
+</div>
+</div>
