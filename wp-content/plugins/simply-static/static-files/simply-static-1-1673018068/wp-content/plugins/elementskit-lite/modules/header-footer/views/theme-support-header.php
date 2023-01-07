7d168e9f041985b5f6f027539aac01da4e5b74e2
Amir Hamza <120237699+engr-amir-hamza@users.noreply.github.com>
2023-01-07 12:53:58 +0500

    m

---
+++ wp-content/plugins/simply-static/static-files/simply-static-1-1673018068/wp-content/plugins/elementskit-lite/modules/header-footer/views/theme-support-header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html><html>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function language_attributes() in D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\elementskit-lite\modules\header-footer\views\theme-support-header.php:2
+Stack trace:
+#0 {main}
+  thrown in <b>D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\elementskit-lite\modules\header-footer\views\theme-support-header.php</b> on line <b>2</b><br>
+</html></html>
