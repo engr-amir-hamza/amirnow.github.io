5d441b09ae0e1d9636fcd0b7feee952aa7ea85aa
Amir Hamza <120237699+engr-amir-hamza@users.noreply.github.com>
2023-01-07 12:51:15 +0500

    mmmm

---
+++ wp-content/plugins/master-addons/inc/modules/dynamic-tags/class-dynamic-tags.php
@@ -0,0 +1,7 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function MasterAddons\Modules\DynamicTags\add_action() in D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\dynamic-tags\class-dynamic-tags.php:16
+Stack trace:
+#0 D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\dynamic-tags\class-dynamic-tags.php(262): MasterAddons\Modules\DynamicTags\JLTMA_Extension_Dynamic_Tags-&gt;__construct()
+#1 D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\dynamic-tags\class-dynamic-tags.php(268): MasterAddons\Modules\DynamicTags\JLTMA_Extension_Dynamic_Tags::get_instance()
+#2 {main}
+  thrown in <b>D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\dynamic-tags\class-dynamic-tags.php</b> on line <b>16</b><br>
