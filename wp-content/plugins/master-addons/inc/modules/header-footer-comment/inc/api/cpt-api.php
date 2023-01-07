e397a05189af42925cf7051fcc606e7d97728d1b
Amir Hamza <120237699+engr-amir-hamza@users.noreply.github.com>
2023-01-07 12:51:31 +0500

    m

---
+++ wp-content/plugins/master-addons/inc/modules/header-footer-comment/inc/api/cpt-api.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;MasterHeaderFooter\JLTMA_Header_Footer_Rest_API&quot; not found in D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\api\cpt-api.php:5
+Stack trace:
+#0 {main}
+  thrown in <b>D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\api\cpt-api.php</b> on line <b>5</b><br>
---
+++ wp-content/plugins/master-addons/inc/modules/header-footer-comment/inc/api/handler-api.php
@@ -0,0 +1,7 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function MasterHeaderFooter\add_action() in D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\api\handler-api.php:21
+Stack trace:
+#0 D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\api\handler-api.php(13): MasterHeaderFooter\Handler_Api-&gt;init()
+#1 D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\api\handler-api.php(42): MasterHeaderFooter\Handler_Api-&gt;__construct()
+#2 {main}
+  thrown in <b>D:\PHP\XAMPP\htdocs\amirnow.github.io\wp-content\plugins\master-addons\inc\modules\header-footer-comment\inc\api\handler-api.php</b> on line <b>21</b><br>
